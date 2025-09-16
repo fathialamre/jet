81f0ae24d585a51e2ec78471cb462e161aec320d
fathialamre <alamre90>
2025-09-16 23:18:20 +0200

    fix : fixed todo form reverpod generator

--- packages/jet/lib/forms/notifiers/jet_form_notifier.dart
+++ packages/jet/lib/forms/notifiers/jet_form_notifier.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
-import 'package:hooks_riverpod/misc.dart';
 import 'package:jet/extensions/build_context.dart';
 import '../../bootstrap/boot.dart';
 import '../../networking/errors/jet_error.dart';
