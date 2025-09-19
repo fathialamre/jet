3a5e6fed0bfdf1890dc63b891b8e287cdf0a8d69
fathialamre <alamre90>
2025-09-19 18:43:29 +0200

    chore: Add .gitignore and remove .ref directory
    - Create comprehensive .gitignore file for Flutter/Dart project
    - Add .ref/ to gitignore to prevent tracking reference files
    - Remove .ref directory from git tracking and local filesystem
    - Clean up repository by removing unnecessary reference files

--- .ref/support-6.x/lib/dart_console/src/ffi/unix/unistd.dart
+++
@@ -1,3 +0,0 @@
-const stdinFileno = 0;
-const stdoutFileno = 1;
-const stderrFileno = 2;
--- .ref/support-6.x/lib/metro/models/metro_project_file.dart
+++
@@ -1,7 +0,0 @@
-/// Model class used for creating files
-class MetroProjectFile {
-  String name;
-  String? creationPath;
-
-  MetroProjectFile(this.name, {this.creationPath});
-}
